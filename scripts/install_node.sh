c9a1cd83750d5baeb7a13dd170c90701c5432746
Curt Tudor <curt.tudor@netfoundry.io>
2020-01-30 10:49:22 -0700

    attempt to bypass nvm install on windows

--- scripts/install_node.sh
+++ scripts/install_node.sh
@@ -7,6 +7,28 @@ fi
 
 NODE_VERSION=$1
 
+echo "OSTYPE is: $OSTYPE"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        # ...
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+        exit 0
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+        exit 0
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+        exit 0
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
+
+
 # if an existing nvm is already installed we need to unload it
 nvm unload || true
 
