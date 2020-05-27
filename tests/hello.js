a23a52a8033ef197b3201c7c28654cc14e661394
rentallect <curt.tudor@netfoundry.io>
2020-05-27 07:19:05 -0400

    checkpoint

--- tests/hello.js
+++ tests/hello.js
@@ -1,7 +1,8 @@
 
 const binary = require('node-pre-gyp');
 const path = require('path')
-const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')), {debug: true});
+// const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')), {debug: true});
+const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')));
 console.log("binding_path is: ", binding_path);
 const ziti = require(binding_path);
 console.log("ziti native addon is: \n", ziti);
