5eac7510ef07212742d334c6633cc71eb337b6a5
rentallect <curt.tudor@netfoundry.io>
2021-10-19 05:24:03 -0400

    update tests

--- tests/hello.js
+++ tests/hello.js
@@ -1,5 +1,5 @@
 
-const binary = require('node-pre-gyp');
+const binary = require('@mapbox/node-pre-gyp');
 const path = require('path')
 // const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')), {debug: true});
 const binding_path = binary.find(path.resolve(path.join(__dirname,'../package.json')));
